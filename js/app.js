46cf42b5f67ba6e7690e0703e0c76f4467dda2e7
Marc Mosca <mosca.marc@bcg.com>
2016-05-31 15:11:13 -0400

    initial site

---
+++ js/app.js
@@ -0,0 +1,24 @@
+$(document).foundation()
+
+$(document).ready(function(){
+$('h1').css({ 'width':'100%', 'text-align':'center' });
+    var h1 = $('h1').height();
+    var h = h1/2;
+    var w1 = $(window).height();
+    var w = w1/2;
+    var m = w - h
+/*$('h1').css("margin-top",m + "px")*/
+/*$('body').css("height",m + "px")*/
+});
+
+$(window).on("orientationchange",function(event){
+$('h1').css({ 'width':'100%', 'text-align':'center' });
+    var h1 = $('h1').height();
+    var h = h1/2;
+    var w1 = $(window).height();
+    var w = w1/2;
+    var m = w - h
+/*$('h1').css("margin-top",m + "px")*/
+/*$('body').css("height",m + "px")*/
+$window.location.reload()
+});
